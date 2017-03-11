02181960e5f5c580be6b6c1b112cad4fc6077150
Neal Fennimore <hi@neal.codes>
2017-03-11 12:18:08 -0800

    Add in error route handling

--- app/index.js
+++ app/index.js
@@ -12,6 +12,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', routes);
+app.use((req, res)=>{
+    res.status(404);
+
+    // respond with json
+    if (req.accepts('json')) {
+        res.send({ error: 'Not found' });
+        return;
+    }
+
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
+});
 
 app.listen(server.port, server.ip, ()=> {
     console.log(`Listening on ${server.ip}:${server.port}`);
