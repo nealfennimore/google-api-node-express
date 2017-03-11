453e67d4554d7e1f99a9835344882f382c4cd73b
Neal Fennimore <hi@neal.codes>
2017-03-11 11:15:51 -0800

    Add in body parser

--- app/index.js
+++ app/index.js
@@ -1,10 +1,15 @@
 require('module-alias/register');
 
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const routes = require('routes');
 const { server } = require('config');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', routes);
 
