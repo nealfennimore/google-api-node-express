2fd62f3d06308a4c400904b5c7ef717959ff7069
Neal Fennimore <hi@neal.codes>
2017-03-11 18:49:10 -0800

    Add in 404 handler

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('routes');
+const _404 = require('./404');
 
 const { env: {SERVER_IP, SERVER_PORT}} = process;
 const app = express();
@@ -13,18 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 app.use('/', routes);
-app.use((req, res)=>{
-    res.status(404);
-
-    // respond with json
-    if (req.accepts('json')) {
-        res.send({ error: 'Not found' });
-        return;
-    }
-
-    // default to plain-text. send()
-    res.type('txt').send('Not found');
-});
+app.use(_404);
 
 app.listen(SERVER_PORT, SERVER_IP, ()=> {
     console.log(`Listening on ${SERVER_IP}:${SERVER_PORT}`);
